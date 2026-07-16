902fd66d4fd1de68683090eba5caf52e4c614a9b
muhffikkri <moehammad.fikr@gmail.com>
2026-07-16 21:38:05 +0700

    Add model_registry.py to define available models and their configurations

---
+++ src/app/model_registry.py
@@ -0,0 +1,51 @@
+import os
+import config as cfg
+
+
+AVAILABLE_MODELS = {
+
+    "multiclass_100":{
+
+        "path": cfg.PRESENTATION_DIR
+
+        "scheme":"multiclass"
+
+        "source_fs":100
+
+    },
+
+    "multiclass_500":{
+
+        "path":...
+
+        "scheme":"multiclass"
+
+        "source_fs":500
+
+    },
+
+    "multilabel_100":{
+
+        "path":...
+
+        "scheme":"multilabel"
+
+        "threshold":[...]
+
+        "source_fs":100
+
+    },
+
+    "multilabel_500":{
+
+        "path":...
+
+        "scheme":"multilabel"
+
+        "threshold":[...]
+
+        "source_fs":500
+
+    }
+
+}
